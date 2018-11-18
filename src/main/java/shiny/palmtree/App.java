21e06e479ce00a080caa87c39fff387615fea6ed
ashrafmulla105 <ashrafmullaaa@gmail.com>
2018-11-18 17:49:14 +0530

    Add files via upload
    First commit

---
+++ src/main/java/shiny/palmtree/App.java
@@ -0,0 +1,13 @@
+package shiny.palmtree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
